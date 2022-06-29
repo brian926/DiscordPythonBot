3ded446e8fd24488c2364a97f7211922ad7efb35
Brian Antunes <brian.antunes@yahoo.com>
2022-06-28 20:27:18 -0400

    Migrated discordBot from Discord.py to Hikari

--- discordBot.py
+++ discordBot.py
@@ -1,7 +1,7 @@
 import os
 import random
-import discord
-from discord.ext import commands
+import hikari
+import lightbulb
 from dotenv import load_dotenv
 import dnd
 
@@ -11,94 +11,65 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 # Start Bot
-# Intents will be discontinued after August 31, 2022
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = lightbulb.BotApp(token=TOKEN, prefix="!", default_enabled_guilds=961761430728736858)
 
-# Classes for Dropdown
-class Dropdown(discord.ui.Select):
-    def __init__(self, resources):
-        self.resources = resources
+@bot.listen(hikari.StartedEvent)
+async def on_started(event: hikari.StartingEvent) -> None:
+    print('Bot has started!')
 
-        # Set the options that will be presented inside the dropdown
-        option = []
-
-        for item in resources:
-            option.append(discord.SelectOption(label=item.capitalize()))
-
-        # The placeholder is what will be shown when no option is chosen
-        # The min and max values indicate we can only pick one of the three options
-        # The options parameter defines the dropdown options. We defined this above
-        super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=1, options=option)
-
-    async def callback(self, interaction: discord.Interaction):
-        # Use the interaction object to send a response message containing
-        # the user's favourite colour or choice. The self object refers to the
-        # Select object, and the values attribute gets a list of the user's 
-        # selected options. We only want the first one.
-        response = dnd.get_endpoints(self.values[0])
-        await interaction.response.send_message(response)
-
-
-# Class for View to add Select to
-class DropdownView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-
-# Bot Section
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} the bot has connected to Discord!')
-
-@bot.command(name='24', help='Test run of the help comand')
-async def twenty_four(ctx):
+@bot.command()
+@lightbulb.command("24", "Test run of the help comand")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def twenty_four(ctx: lightbulb.Context) -> None:
     resources = dnd.list_endpoints()
     view = DropdownView().add_item(Dropdown(resources))
 
     # Sending a message containing our view
     await ctx.send('Pick your favourite colour:', view=view)
 
-
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
+@bot.command()
+@lightbulb.option('number_of_dice', 'The number of dices', type=int)
+@lightbulb.option('number_of_sides', 'The number of sides', type=int)
+@lightbulb.command("roll_dice", "Simulates rolling dice.")
+@lightbulb.implements(lightbulb.PrefixCommand)
+async def roll(ctx: lightbulb.Context) -> None:
     dice = [
-        str(random.choice(range(1, number_of_sides +1)))
-        for _ in range(number_of_dice)
+        str(random.choice(range(1, ctx.options.number_of_sides +1)))
+        for _ in range(ctx.options.number_of_dice)
         ]
-    await ctx.send(', '.join(dice))
+    await ctx.respond(', '.join(dice))
 
-@bot.command(name='Resources', help='List out the different Resources to use')
-async def list_options(ctx):
+@bot.command()
+@lightbulb.option('test', 'The number of dices', type=int)
+@lightbulb.command("resources", "List out the different Resources to use")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def list_options(ctx: lightbulb.Context) -> None:
     print("Printing Resources")
     response = dnd.list_endpoints()
-    await ctx.send(response)
+    await ctx.respond(response)
 
-@bot.command(name='Options', help='List out the different options for a resource')
-async def list_options(ctx, endpoint: str):
+@bot.command()
+@lightbulb.option('test2', 'The number of dices', type=int)
+@lightbulb.command("options", "List out the different options for a resource")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def list_options2(ctx: lightbulb.Context) -> None:
     print("Printing Options")
     response = dnd.get_endpoints(endpoint)
-    await ctx.send(response)
+    await ctx.respond(response)
 
-@bot.command(name='Details', help='Get details on an Option from the Resources')
-async def list_options(ctx, resource: str, option: str):
+@bot.command()
+@lightbulb.option('test3', 'The number of dices', type=int)
+@lightbulb.command("details", "Get details on an Option from the Resources")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def list_options3(ctx: lightbulb.Context) -> None:
     print("Printing Details")
     response = dnd.get_details(resource, option)
-    await ctx.send(response)
+    await ctx.respond(response)
 
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx):
+    await ctx.respond('Pong!')
 
-@bot.command(name='create-channel')
-@commands.has_role('admin')
-async def create_channel(ctx, channel_name='real-python'):
-    guild = ctx.guild
-    existing_channel = discord.utils.get(guild.channels, name=channel_name)
-    if not existing_channel:
-        print(f'Creating a new channel : {channel_name}')
-        await guild.create_text_channel(channel_name)
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
-
-bot.run(TOKEN)
+bot.run()
